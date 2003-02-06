83a97de22566aa169dd2538bd00bf1d7b3514119
Mikael Ståldal <mikael@staldal.nu>
2003-02-06 19:14:17 +0000

    *** empty log message ***

---
+++ src/nu/staldal/lsp/expr/ConditionalExpr.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2003, Mikael Ståldal
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the author nor the names of its contributors
+ * may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ * Note: This is known as "the modified BSD license". It's an approved
+ * Open Source and Free Software license, see
+ * http://www.opensource.org/licenses/
+ * and
+ * http://www.gnu.org/philosophy/license-list.html
+ */
+
+package nu.staldal.lsp.expr;
+
+import nu.staldal.lsp.LSPExpr;
+
+/**
+ * A if-then-else expression.
+ */
+public class ConditionalExpr extends LSPExpr
+{
+    protected LSPExpr testExpr;
+    protected LSPExpr thenExpr;
+	protected LSPExpr elseExpr;
+
+	public ConditionalExpr(LSPExpr testExpr, LSPExpr thenExpr, LSPExpr elseExpr)
+	{
+		this.testExpr = testExpr;
+		this.thenExpr = thenExpr;
+		this.elseExpr = elseExpr;
+	}
+
+	public LSPExpr getTest()
+	{
+		return testExpr;
+	}
+
+	public LSPExpr getThen()
+	{
+		return thenExpr;
+	}
+
+	public LSPExpr getElse()
+	{
+		return elseExpr;
+	}
+    
+
+	public String toString()
+	{
+		return "ConditionalExpr(" + testExpr + "," + thenExpr + "," + elseExpr + ")";
+	}
+
+}
+
