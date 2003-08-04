f984cb71b1970b2419d152907311cdb9ed305192
Mikael Ståldal <mikael@staldal.nu>
2003-08-04 10:01:13 +0000

    BCEL

--- bin/lspc.cmd
+++ bin/lspc.cmd
@@ -4,4 +4,4 @@ rem Windows and OS/2 batch script for running lspc
 
 rem SET LAGOON_HOME=D:\lagoon
 
-java -classpath %CLASSPATH%;%LAGOON_HOME%\lsp.jar;%LAGOON_HOME%\xmlutil.jar nu.staldal.lsp.LSPCompilerCLI %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -classpath %CLASSPATH%;bcel.jar;%LAGOON_HOME%\lsp.jar;%LAGOON_HOME%\xmlutil.jar nu.staldal.lsp.LSPCompilerCLI %1 %2 %3 %4 %5 %6 %7 %8 %9
