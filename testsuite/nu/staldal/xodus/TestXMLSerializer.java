9a860511320ec44040fdc813c6baf3a8883d0c13
Mikael Ståldal <mikael@staldal.nu>
2006-07-10 16:58:17 +0000

    Removed unused import

--- testsuite/nu/staldal/xodus/TestXMLSerializer.java
+++ testsuite/nu/staldal/xodus/TestXMLSerializer.java
@@ -11,7 +11,6 @@ import javax.xml.transform.stream.StreamResult;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.xml.sax.SAXException;
 import org.xml.sax.helpers.AttributesImpl;
 
 /**
