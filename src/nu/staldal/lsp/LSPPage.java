b5c31fdd0d5af9d5e8e9a174070571b5b2e992b7
Mikael Ståldal <mikael@staldal.nu>
2006-10-31 16:01:32 +0000

    Version 1.7.1

--- src/nu/staldal/lsp/LSPPage.java
+++ src/nu/staldal/lsp/LSPPage.java
@@ -57,7 +57,7 @@ public interface LSPPage
     /**
      * Current version of LSP.
      */
-    public static final String LSP_VERSION_NAME = "1.7";
+    public static final String LSP_VERSION_NAME = "1.7.1";
     
     /**
      * Version number to check compatibility between runtime and compiled pages.
