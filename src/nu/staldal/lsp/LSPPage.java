89f87e0fafc0b1f9de632482b8b7c06c2086587d
Mikael Ståldal <mikael@staldal.nu>
2008-05-12 12:31:37 +0000

    Bump version number

--- src/nu/staldal/lsp/LSPPage.java
+++ src/nu/staldal/lsp/LSPPage.java
@@ -57,7 +57,7 @@ public interface LSPPage
     /**
      * Current version of LSP.
      */
-    public static final String LSP_VERSION_NAME = "1.7.3";
+    public static final String LSP_VERSION_NAME = "1.8";
     
     /**
      * Version number to check compatibility between runtime and compiled pages.
