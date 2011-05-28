41ea4ce6ea2eefa390ea46cd3ba7eff82865d1e9
Mikael Ståldal <mikael@staldal.nu>
2011-05-28 22:53:21 +0200

    Moving ZeroTemplate to own repository

--- zt/src/main/python/test.py
+++
@@ -1,9 +0,0 @@
-from ZeroTemplate import ZeroTemplate
-import os
-import sys
-
-ZeroTemplate.setPageResolver(lambda (pageName): file(pageName+'.zt'))
-os.chdir('../testsuite/ztPages')
-doc = ZeroTemplate.loadPage('unicode')
-doc.render(sys.stdout, None)
-
--- zt/src/test/java/nu/staldal/zt/TestBean.java
+++
@@ -1,9 +0,0 @@
-package nu.staldal.zt;
-
-public class TestBean {
-    public String foo = "-FOO-";
-
-    public String bar = "-BAR-";
-
-    public String baz = "-BAZ-";
-}
